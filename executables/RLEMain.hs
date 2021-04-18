20fd99e40da16bc564e6b178a3ee3ee8e65b9fea
Saatvik Shah <saatvikshah1994@gmail.com>
2021-04-18 01:59:26 -0500

    Use optparse-applicative for command line parsing

--- executables/RLEMain.hs
+++ executables/RLEMain.hs
@@ -3,26 +3,45 @@ module Main where
 import FileIO
 import RLECore
 import System.Environment (getArgs)
+import Options.Applicative
+import Data.Semigroup ((<>))
 import Data.List.Extra
 
-processArgs :: [String] -> IO ()
-processArgs args = case args of
-    [] -> pure ()
-    ("--compress":file_arg:xs) -> do
-        file_contents <- readFileContents file_arg
-        let compressed_content = runLengthEncode file_contents
-        let compressed_fname = file_arg ++ ".compressed"
-        writeContentToFile compressed_fname compressed_content
-        processArgs xs
-    ("--decompress":file_arg:xs) -> do
-        let decompressed_fname = case stripSuffix ".compressed" file_arg of
-                Just x  -> x
-                Nothing -> error "Invalid input filename - should be suffixed with .compressed"
-        file_contents <- readFileContents file_arg
-        let decompressed_content = runLengthDecode file_contents
-        writeContentToFile decompressed_fname decompressed_content
-        processArgs xs
-    _ -> error "Unrecognized arguments"
+data Args = Args
+    {   argsCompress   :: Maybe String,
+        argsDecompress :: Maybe String
+    }
+
+parseArgs :: Parser Args
+parseArgs = Args
+    <$> (optional $ strOption
+        $  long "compress"
+        <> short 'c'
+        <> help "file to be compressed")
+    <*> (optional $ strOption
+        $   long "decompress"
+        <>  short 'd'
+        <>  help "file to be decompressed")
+
+processArgs :: Args -> IO ()
+processArgs (Args (Just file_arg) Nothing) = do
+    file_contents <- readFileContents file_arg
+    let compressed_content = runLengthEncode file_contents
+    let compressed_fname = file_arg ++ ".compressed"
+    writeContentToFile compressed_fname compressed_content
+processArgs (Args Nothing (Just file_arg)) = do    
+    let decompressed_fname = case stripSuffix ".compressed" file_arg of
+            Just x  -> x
+            Nothing -> error "Invalid input filename - should be suffixed with .compressed"
+    file_contents <- readFileContents file_arg
+    let decompressed_content = runLengthDecode file_contents
+    writeContentToFile decompressed_fname decompressed_content
+processArgs _ = return ()
 
 main :: IO ()
-main = getArgs >>= processArgs
+main = execParser opts >>= processArgs
+    where
+        opts = info (parseArgs <**> helper)
+            (   fullDesc
+            <>  progDesc "Compress or decompress a file using run-length encoding."
+            <>  header "rle-compressor" )
